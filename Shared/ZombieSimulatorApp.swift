204b29cd83d8deee70f6e927e2ef997adf452d85
Spencer Lee <slee216@hawk.iit.edu>
2021-04-02 10:36:44 -0500

    Initial Commit

---
+++ Shared/ZombieSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZombieSimulatorApp.swift
+//  Shared
+//
+//  Created by Spencer Lee on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ZombieSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
